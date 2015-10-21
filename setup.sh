6aed107a80ca256c815a9788c24253d31f50c6f8
Dennis Ideler <ideler.dennis@gmail.com>
2015-10-21 01:01:11 +0100

    Abort setup if not OS X

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash -xe
 
+if [[ "$(uname)" != "Darwin" ]]; then
+  echo 'This setup only runs on OS X machines'
+  exit 1
+fi
+
 mkdir -p ~/github
 git clone git@github.com:dideler/setup-osx.git ~/github/setup-osx
 
