b2c1c356f634063492fdca26f6f5edcd2c80705b
Dennis Ideler <ideler.dennis@gmail.com>
2015-10-26 14:28:18 +0000

    Also install dotfiles

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ To get started, run
 curl -s https://raw.githubusercontent.com/dideler/dotfiles/master/setup.sh | bash
 ```
 
+It will also [install the dotfiles][dotfiles].
+
 Run at your own risk!
 
 ---
-
-A good next step would be to [install the dotfiles][dotfiles].
 
 [dotfiles]: https://github.com/dideler/dotfiles
--- setup.sh
+++ setup.sh
@@ -8,15 +8,20 @@ fi
 xcode-select --install # Command Line Tools
 
 mkdir -p ~/github
-git clone git@github.com:dideler/setup-osx.git ~/github/setup-osx
 
-pushd ~/github/setup-osx
-  # TODO: Some of these can probably run parallel in child processes.
-  source settings/osx-defaults
-  source package-managers/homebrew
-  source package-managers/npm
-  source package-managers/gems
-  source languages/ruby
-  source languages/node
-  source shells/fish
+git clone git@github.com:dideler/dotfiles.git ~/github/dideler/dotfiles
+pushd ~/github/dideler/dotfiles
+  bash link_dotfiles -f
+popd
+
+git clone git@github.com:dideler/setup-osx.git ~/github/dideler/setup-osx
+pushd ~/github/dideler/setup-osx
+  # TODO: Some of these can probably run parallel in child processes. And maybe use nohup?
+  bash settings/osx-defaults
+  bash package-managers/homebrew
+  bash package-managers/npm
+  bash package-managers/gems
+  bash languages/ruby
+  bash languages/node
+  bash shells/fish
 popd
