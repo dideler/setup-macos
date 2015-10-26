eb35dfd92625eef166e8709744a2ef31bbc5a5fd
Dennis Ideler <ideler.dennis@gmail.com>
2015-10-26 14:48:36 +0000

    Run scripts in sequence and only start next one on successful exit

--- setup.sh
+++ setup.sh
@@ -16,12 +16,12 @@ popd
 
 git clone git@github.com:dideler/setup-osx.git ~/github/dideler/setup-osx
 pushd ~/github/dideler/setup-osx
-  # TODO: Some of these can probably run parallel in child processes. And maybe use nohup?
-  bash settings/osx-defaults
-  bash package-managers/homebrew
-  bash package-managers/npm
-  bash package-managers/gems
-  bash languages/ruby
-  bash languages/node
-  bash shells/fish
+  # TODO: Some of these can probably run in parallel. And maybe use nohup?
+  bash settings/osx-defaults \
+  && bash package-managers/homebrew \
+  && bash package-managers/npm \
+  && bash package-managers/gems \
+  && bash languages/ruby \
+  && bash languages/node \
+  && bash shells/fish
 popd
