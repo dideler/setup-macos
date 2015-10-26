f7b99616d60408cbeea4766360c1b1651721a63b
Dennis Ideler <ideler.dennis@gmail.com>
2015-10-26 15:02:00 +0000

    Switch to HTTPS clones

--- setup.sh
+++ setup.sh
@@ -9,12 +9,13 @@ xcode-select -p 1>/dev/null || xcode-select --install # Install Command Line Too
 
 mkdir -p ~/github
 
-git clone git@github.com:dideler/dotfiles.git ~/github/dideler/dotfiles
+# Cloning with HTTPS to avoid SSH setup. You may want to change the remote URL to SSH later.
+git clone https://github.com/dideler/dotfiles.git ~/github/dideler/dotfiles
 pushd ~/github/dideler/dotfiles
   bash link_dotfiles -f
 popd
 
-git clone git@github.com:dideler/setup-osx.git ~/github/dideler/setup-osx
+git clone https://github.com/dideler/setup-osx.git ~/github/dideler/setup-osx
 pushd ~/github/dideler/setup-osx
   # TODO: Some of these can probably run in parallel. And maybe use nohup?
   bash settings/osx-defaults \
