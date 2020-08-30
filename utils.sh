e14a109c3afceb958d31961aa94d72fbdb3b55c2
Dennis Ideler <ideler.dennis@gmail.com>
2020-08-30 17:19:32 +0100

    Print failure on error
    Any script that sources utils, will now have failure logging.

--- utils.sh
+++ utils.sh
@@ -4,12 +4,19 @@ BLACK=0 RED=1 GREEN=2 YELLOW=3 BLUE=4 MAGENTA=5 CYAN=6 WHITE=7
 font_colour() { tput setaf "$1"; }
 colour_print() { echo -e "${BOLD}$(font_colour "$1")${*:2}${NORMAL}"; }
 
+trap 'on_error $(basename $0) $LINENO' ERR
+
 function log_info {
   echo -e "$(colour_print $GREEN '[INFO]') $*"
 }
 
 function log_error {
   >&2 echo -e "$(colour_print $RED '[ERROR]') $*"
+}
+
+function on_error {
+  log_error "$1 failed on line $2"
+  if [[ $ERR_CONTEXT != "" ]]; then log_error "Context: $ERR_CONTEXT"; fi
 }
 
 function is_available() {
