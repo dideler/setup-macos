5cbc8576782956ebe4dd3ceedd65e92aa5172e2e
Dennis Ideler <ideler.dennis@gmail.com>
2020-08-30 17:18:46 +0100

    Add log levels to utils

--- utils.sh
+++ utils.sh
@@ -1,3 +1,17 @@
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+BLACK=0 RED=1 GREEN=2 YELLOW=3 BLUE=4 MAGENTA=5 CYAN=6 WHITE=7
+font_colour() { tput setaf "$1"; }
+colour_print() { echo -e "${BOLD}$(font_colour "$1")${*:2}${NORMAL}"; }
+
+function log_info {
+  echo -e "$(colour_print $GREEN '[INFO]') $*"
+}
+
+function log_error {
+  >&2 echo -e "$(colour_print $RED '[ERROR]') $*"
+}
+
 function is_available() {
   command -v "$1" >/dev/null
   return $?
