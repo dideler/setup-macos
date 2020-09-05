15a67ec84d503f84feea257c758929c74f856b72
Dennis Ideler <ideler.dennis@gmail.com>
2020-09-05 15:32:01 +0100

    Make errors more noticeable

--- utils.sh
+++ utils.sh
@@ -1,17 +1,18 @@
 BOLD=$(tput bold)
-NORMAL=$(tput sgr0)
+RESET=$(tput sgr0)
 BLACK=0 RED=1 GREEN=2 YELLOW=3 BLUE=4 MAGENTA=5 CYAN=6 WHITE=7
-font_colour() { tput setaf "$1"; }
-colour_print() { echo -e "${BOLD}$(font_colour "$1")${*:2}${NORMAL}"; }
+fg_color() { tput setaf "$1"; }
+bg_color() { tput setab "$1"; }
+print_bold() { echo -e "${BOLD}$(fg_color "$1")$(bg_color "$2")${*:3}${RESET}"; }
 
 trap 'on_error $(basename $0) $LINENO' ERR
 
 function log_info {
-  echo -e "$(colour_print $GREEN '[INFO]') $*"
+  echo -e "$(print_bold $GREEN $BLACK '[INFO]') $*"
 }
 
 function log_error {
-  >&2 echo -e "$(colour_print $RED '[ERROR]') $*"
+  >&2 echo -e "$(print_bold $RED $YELLOW '[ERROR]') $*"
 }
 
 function on_error {
